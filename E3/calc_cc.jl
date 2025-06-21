8d9b36d9b14b8525a3f647766a4c1a0f10b9f4ed
naszhu <naszhu@gmail.com>
2025-06-21 21:48:24 +0800

    refactor(model-e3): p(match) calculation
    added a 4 lines of the calculation of the new thoughts
    
    Refs #31

--- E3/calc_cc.jl
+++ E3/calc_cc.jl
@@ -21,3 +21,19 @@ p_now_correct = p_orig_correct * p_now_correct_given_orig_correct +
 println("P(now correct | orig correct)   = ", p_now_correct_given_orig_correct)
 println("P(now correct | orig incorrect) = ", p_now_correct_given_orig_incorrect)
 println("P(now correct)                  = ", p_now_correct)
+
+
+# calculation of the second part of the term in the equation of calculating p(match) when considering random generation "accidently" makes the feature match 
+
+
+# see manual draft in the issue 
+g = 0.02
+
+# before is when no strenghten
+a_before = 0.3*(1-g)*0.6*g
+
+# after is when strenghten
+a_after = 0.3*(1-g)* (0.6*g*0.8 + 0.8*0.4 + 0.2*0.6*g)
+
+println("a_before = ", a_before)
+println("a_after = ", a_after)
