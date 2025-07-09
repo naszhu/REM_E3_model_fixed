a8951b6099a4b3a75084f776bb7c369fc89bf07a
naszhu <naszhu@gmail.com>
2025-07-09 21:00:58 +0800

    fintune(model-e3): how to make SO the worst CR? adjust p_old val

--- E3/constants.jl
+++ E3/constants.jl
@@ -246,9 +246,9 @@ start_and_end = [0.2, 0.5]
 asymptotic_vals =  LinRange(start_and_end[1], start_and_end[2], ilist_switch_stop_at-1)
 
 p_switch_toListOrgin = vcat(0,asymptotic_vals, asymptotic_vals[end]*ones(n_lists-ilist_switch_stop_at)...)#probabiltiy of switch (or can say, recall LOR) from familarity to recall, from familarity to knowing "List of Origin"
-p_old_with_ListOrigin_SOn = 0.35
+p_old_with_ListOrigin_SOn = 0.65
 # p_old_with_ListOrigin_Tn_Fn = 0.5 #PO+ 
-p_old_with_ListOrigin_Fn = 0.45 
+p_old_with_ListOrigin_Fn = 0.25 
 p_old_with_ListOrigin_Tn = 0.01 #PO++ (prior target have lowest-make sense)
 
 # context_threshold_filter = 0
